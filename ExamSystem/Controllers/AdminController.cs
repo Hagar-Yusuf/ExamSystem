3eed6858bca33ec5779e754b09f2fa6efa94db68
Hagar-yusuf306 <hagaryusuf1@gmail.com>
2025-07-13 20:13:30 +0300

    initial commit

---
+++ ExamSystem/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using ExamSystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        public ExamDBContext Db { get; }
+
+        public AdminController(ExamDBContext db)
+        {
+            Db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            var users = Db.Users.ToList();
+            return Ok(users);
+        }
+
+    }
+}
